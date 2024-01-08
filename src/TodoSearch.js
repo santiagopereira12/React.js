337d9d5880a1a58d73f5afa4d59988c7a2b469e2
santiagopereira12 <spereira98@misena.edu.co>
2024-01-08 18:08:15 -0500

    Se realiza implementacion de onClick y onSearch

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,16 @@ import './TodoSearch.css'
 
 function TodoSearch(){
     return (
-        <input placeholder="Crear pagina." className="TodoSearch"/>
+        <input placeholder="Crear pagina." 
+        className="TodoSearch"
+        onChange={
+            (event)=>{
+                console.log('Escribiste en el TodoSearch')
+                console.log(event.target)
+                console.log(event.target.value)
+            }
+        }
+        />
     );
 }
 
